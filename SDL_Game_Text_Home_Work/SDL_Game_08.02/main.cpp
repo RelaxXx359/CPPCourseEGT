5f64c459b59392ac303ea36fddbae0b3a6162e7b
Dobrolyub Velinov <dvelinov_kn@abv.bg>
2024-02-08 17:37:32 +0200

    Add home work

---
+++ SDL_Game_Text_Home_Work/SDL_Game_08.02/main.cpp
@@ -0,0 +1,26 @@
+﻿//main.cpp
+#include "Game.h"
+#include <iostream>
+
+Game* game = NULL;
+const int WINDOW_WIDTH = 1280;
+const int WINDOW_HEIGHT = 720;
+
+int main(int argc, char* argv[]) {
+
+	game = new Game();
+	game->init("My new window",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		WINDOW_WIDTH, WINDOW_HEIGHT,
+		SDL_WINDOW_RESIZABLE);
+	//функция за зареждане на текста
+	game->ttf_init();
+	while (game->isRunning()) {
+		game->handleEvents();
+		game->update();
+		game->render();
+	}
+	game->clean();
+	return 0;
+}
