749237d64a97aff2c1327bbb5b0053f858ae8217
Dobrolyub Velinov <dvelinov_kn@abv.bg>
2024-02-16 11:42:38 +0200

    Add home work

---
+++ CodingTasks_Inheritance_Homework/CodingTasks_Inheritance_Homework/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+class Shape {
+public:
+	Shape(const std::string color) : color(color){}
+	void draw() const;
+
+
+protected:
+	std::string color;
+};
+
+class Circle : public Shape {
+public:
+	Circle(const std::string color ) : Shape(color){}
+	void draw() const {
+		std::cout << "Drawing a "<< color << " circle" << std::endl;
+	}
+
+};
+class Rectangle :public Shape {
+public:
+	Rectangle(const std::string color) : Shape(color){}
+	void draw() const  {
+		std::cout << "Drawing a " << color <<  " rectangle" << std::endl;
+	}
+
+};
+
+int main() {
+
+	Circle c1("red");
+	Rectangle r1("blue");
+
+	c1.draw();
+	r1.draw();
+
+	return 0;
+}
