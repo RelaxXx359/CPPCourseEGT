caf64292995ce6ec1cf96f51828baf79bf866de0
Dobrolyub Velinov <dvelinov_kn@abv.bg>
2024-02-16 10:48:54 +0200

    Add first task

---
+++ CodingTasks_Classes_Homework/CodingTasks_Classes_Homework/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+class Book {
+public:
+	// конструктор за инициализиране на атрибути
+	Book(std::string title, std::string author, int pages) :
+		title(title), author(author), pages(pages) {}
+
+	// Функция за отпечатване
+	void display() {
+		std::cout << "Title: " << title << std::endl;
+		std::cout << "Autor: " << author << std::endl;
+		std::cout << "Pages: " << pages << std::endl;
+	}
+
+private:
+	std::string title;   // Заглавие на книгата
+	std::string author;  // Автор на книгата
+	int pages; // Брой страници на книгата
+};
+
+
+int main() {
+
+	Book book1("Pod igoto", "Ivan Vazov", 58);
+
+	book1.display();
+
+	
+	return 0;
+}
